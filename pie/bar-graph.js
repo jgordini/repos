dce21e9a00ca1bf8f23c0f57ba316812281c5204
jgordini <jgordini@uab.edu>
2022-07-04 19:13:54 -0500

    yup

---
+++ pie/bar-graph.js
@@ -0,0 +1,11 @@
+import * as d3 from "./node_modules/d3/dist/d3.js";
+// create svg element:
+var svg = d3.select("#circle").append("svg").attr("width", 200).attr("height", 200)
+
+// Add the path using this helper function
+svg.append('circle')
+    .attr('cx', 100)
+    .attr('cy', 100)
+    .attr('r', 50)
+    .attr('stroke', 'black')
+    .attr('fill', '#69a3b2');
